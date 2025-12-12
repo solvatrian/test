0d882903bea599a9a1339b8b3a92390e52d0683f
craton <cration917@gmail.com>
2025-12-12 21:12:10 +0800

    Set up ui logic

--- index.js
+++ index.js
@@ -1,41 +1,60 @@
-// console.log("Hel");
-
-// randomly returns "rock", "paper", "scissors"
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice() {
-    return prompt("Input the choice", "rock").toLowerCase();
+function getHumanChoice(choice) {
+    switch (choice) {
+        case rockBtn.id:
+            return 'rock';
+        case scissorsBtn.id:
+            return 'scissors';
+        case paperBtn.id:
+            return 'paper';
+    }
 }
 
-function playGame() {
-    function playRound(human, computer) {
-        if (human === computer){
-            console.log("Tie!");
-        }
-        else if (human === "rock" && computer === "scissors" ||
-            human === "scissors" && computer === "paper" ||
-            human === "paper" && computer === "rock") {
-            console.log(`You win! ${human} beats ${computer}.`);
-            humanScore += 1;
-        }
-        else {
-            console.log(`You lose! ${computer} beats ${human}.`);
-            computerScore += 1;
-        }
+function isGetScore5() {
+    if (computerScore >= 5 || humanScore >= 5) {
+        return true;
     }
-    var computerScore = 0;
-    var humanScore = 0;
-
-    for (var i = 0; i < 5; i++) {
-        var humSelection = getHumanChoice();
-        var cptSelection = getComputerChoice();
-        playRound(humSelection, cptSelection);
-    }
-    console.log(`The result is ${humanScore}:${computerScore}, well play!`);
+    return false;
 }
 
-playGame();
-// console.log(choice);
+function printGameResult() {
+    divRes.textContent = `The result is ${humanScore}:${computerScore}, well play!`;
+}
+
+function playRound(human, computer) {
+    if (human === computer) {
+        divRes.textContent = "Tie!";
+    }
+    else if (human === "rock" && computer === "scissors" ||
+        human === "scissors" && computer === "paper" ||
+        human === "paper" && computer === "rock") {
+        divRes.textContent = `You win! ${human} beats ${computer}.`;
+        humanScore += 1;
+    }
+    else {
+        divRes.textContent = `You lose! ${computer} beats ${human}.`;
+        computerScore += 1;
+    }
+}
+
+const rockBtn = document.querySelector('button#rock');
+const scissorsBtn = document.querySelector('button#scissors');
+const paperBtn = document.querySelector('button#paper');
+const divRes = document.querySelector('div#result');
+const divBtns = document.querySelector('div#btn');
+let computerScore = 0;
+let humanScore = 0;
+
+divBtns.addEventListener('click', (e) => {
+    let target = e.target;
+    let humSelection = getHumanChoice(target.id);
+    var cptSelection = getComputerChoice();
+    playRound(humSelection, cptSelection);
+    if (isGetScore5()) {
+        printGameResult();
+    }
+})
